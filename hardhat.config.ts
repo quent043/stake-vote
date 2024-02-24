9773e08f77d00b5b863cb5b9c4806f0b4d3bcc5b
Quentin D.C <quent043@hotmail.com>
2024-02-24 15:41:49 +0400

    Updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import '@nomicfoundation/hardhat-toolbox'
+import '@openzeppelin/hardhat-upgrades'
+import '@openzeppelin/hardhat-defender'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
