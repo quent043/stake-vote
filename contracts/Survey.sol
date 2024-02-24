4d10f2ac1da078bab1e2d8fc24311eaf684e6e36
Quentin D.C <quent043@hotmail.com>
2024-02-24 10:35:42 +0400

    Added Survey Contract

---
+++ contracts/Survey.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "../Interfaces/IStaking.sol";
+
+contract Survey is UUPSUpgradeable, Initializable, AccessControlUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+
+    // =========================== Variables & Declarations ==============================
+
+    uint256 public SURVEY_COST = 1 ether;
+
+    CountersUpgradeable.Counter public nextSurveyId;
+
+    IStaking public stakingContract;
+
+    IVoting public votingContract;
+
+    struct Survey {
+        address owner;
+        string descriptionUri;
+        bool active;
+        address tokenAddress;
+        uint256 yesCount;
+        uint256 noCount;
+        uint256 minimumStake;
+        uint256 endTimestamp;
+        mapping(address => Vote) votes;
+    }
+
+    // =========================== Mappings ==============================
+
+
+    mapping(uint256 => Survey) public surveys;
+
+
+    // =========================== Events ==============================
+
+    event SurveyCreated(uint256 surveyId, address owner, string descriptionUri, address tokenAddress, uint256 minimumStake, uint256 endTimestamp);
+
+    event SurveyCancelled(uint256 surveyId);
+
+    event SurveyCostUpdated(uint256 newCost);
+
+
+    // =========================== View functions ==============================
+
+    function getSurvey(uint256 surveyId) external view returns (Survey memory) {
+        return surveys[surveyId];
+    }
+
+
+    // =========================== Initializers ==============================
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @notice First initializer function
+     * @param
+     */
+    function initialize(address _talentLayerPlatformIdAddress, address _votingContractAddress) public initializer {
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        // Increment counter to start survey ids at index 1
+        nextSurveyId.increment();
+        stakingContract = IStakingContract(_stakingContractAddress);
+        votingContract = IStakingContract(_votingContractAddress);
+    }
+
+    // =========================== Public functions ==============================
+
+    function updateSurveyCost (uint256 _newCost) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        SURVEY_COST = _newCost;
+
+        emit SurveyCostUpdated(_newCost);
+    }
+
+    function createSurvey(
+        address _tokenAddress,
+        string calldata _descriptionUri,
+        address _tokenAddress,
+        uint256 _minimumStake,
+        uint256 _durationInDays
+    ) external {
+        require(stakingContract.isTokenAllowed(token) == true, "Token not allowed");
+        require(msg.value == SURVEY_COST, "Incorrect amount of ETH for survey creation");
+
+        uint256 currentSurveyId = nextSurveyId.current();
+
+        surveys[currentSurveyId] = Survey({
+            owner: msg.sender,
+            descriptionUri: _descriptionUri,
+            active: true,
+            tokenAddress: _tokenAddress,
+            yesCount: 0,
+            noCount: 0,
+            minimumStake: _minimumStake,
+            endTimestamp: block.timestamp + _durationInDays * 1 days
+        });
+
+        nextSurveyId.increment();
+
+        emit SurveyCreated(currentSurveyId, msg.sender, _descriptionUri, _tokenAddress, _minimumStake, block.timestamp + _durationInDays * 1 days);
+    }
+
+    function cancelSurvey(uint256 _surveyId) external {
+        require(surveys[_surveyId].owner == msg.sender, "Not the owner");
+        require(surveys[_surveyId].active, "Survey not active");
+        surveys[_surveyId].active = false;
+
+        emit SurveyCancelled(_surveyId);
+    }
+
+
+    function withdraw() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+
+    // =========================== Private functions ==============================
+    // =========================== Internal functions ==============================
+
+    /**
+     * @notice Function that revert when `_msgSender()` is not authorized to upgrade the contract. Called by
+     * {upgradeTo} and {upgradeToAndCall}.
+     * @param newImplementation address of the new contract implementation
+     */
+    function _authorizeUpgrade(address newImplementation) internal override(UUPSUpgradeable) onlyRole(DEFAULT_ADMIN_ROLE) {}
+
+}
